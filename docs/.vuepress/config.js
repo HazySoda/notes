29c9488a2712f46857d637e78acb3bc4bd0852a3
KidneyFlower <753475777@qq.com>
2018-11-14 23:37:52 +0800

    初始化项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'WildNode\'s Notes',
+  description: 'WildNode\'s note book',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: 'GitHub', link: 'https://github.com/KidneyFlower/notes' }
+    ],
+    sidebar: 'auto'
+  }
+}
